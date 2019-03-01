11b6b00701a548518bd68dad632ad913ebb60cfb
marywedd <weddm037@newschool.edu>
2019-03-01 10:01:03 -0500

    added yinyang

---
+++ projects/java/main.js
@@ -0,0 +1,44 @@
+console.log('Mary');
+console.log(9);
+console.log('9');
+console.log(true);
+console.log(['m,a,r,y']);
+console.log(['string',3,true])
+console.log('mary'+'wedd')
+console.log(8+2)
+var myArray = ['m', 'a', 'r', 'y'];
+var firstItem = myArray[0]; // 'm'
+var lastItem = myArray[3];  // 'y'
+
+lastItem = myArray[myArray.length - 1]; // 'y'
+var MaryP =('supercalifragilisticexpialidocious');
+console.log (MaryP.length);
+
+console.log(4==4)
+
+if (students=16) {}
+	// An example
+var students = [
+  'Amaya',
+  'Sierra',
+  'Colin',
+  'Trudy',
+  'Aeva',
+  'Maddie',
+  'Scarlet',
+  'Phoebe',
+  'Cecilia',
+  'Gavin',
+  'Aileen',
+  'Simran',
+  'Julia',
+  'Amal',
+  'Mary',
+  'Xiyue'
+];
+
+for ( var i = 0; i < students.length; i++ ) {
+  console.log( students[i] + ' is in index ' + i );
+}
+
+console.log(4==4)
