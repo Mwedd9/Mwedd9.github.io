f2bb6649c5f001709e8fc422b9c9d56a8edfb2ac
marywedd <weddm037@newschool.edu>
2019-03-12 10:18:23 -0400

    revised homepage

---
+++ jQuery/toggle-text/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  // Your interactions go here
+  $('.button').click(function)(){
+  	console.log('i clicked it');
+
+  	if ($)
+
+  });
+});
--- projects/java/main.js
+++ projects/java/main.js
@@ -42,3 +42,25 @@ for ( var i = 0; i < students.length; i++ ) {
 }
 
 console.log(4==4)
+
+
+
+// Create a conditional statement that does the following:
+// If all students are here log a message that says all the students are in class
+// Otherwise, log a message that says not everyone is here
+// Create a for loop that loops through the array of characters in your first name and logs each character.
+
+// Create a conditional statement that checks to see if the current time is between 7pm and 9:40pm.
+
+// Create a conditional statement with multiple conditions that checks to see what the current date it is, and logs a unique message for each day of the week.
+
+// Create 4 arrays consisting of the following items. Define variables to store each of the 4 arrays.
+// Your favorite 3 foods
+// Your favorite 3 animals
+// Your favorite 3 places
+// Your favorite 3 pokemon
+// Create an array of arrays consisting of the variables from the previous question. Define a variable to hold this array of arrays.
+
+// Loop through your array of arrays and log the value of the each item in the array. You should be looping through 4 things, and each value you log should be an array. For example, the first item logged would be an array containing 3 foods, the second item would be an array containing 3 animals, etc.
+
+// Create a nested for loop that loops through the array of your favorite items and logs each individual item. The first loop should log the category of the item, while the nested loop should log each individual item in that category.
