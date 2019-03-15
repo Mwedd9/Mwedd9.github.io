7c5d85c5b7d45ab73ce240723c9f31c5934d3915
marywedd <weddm037@newschool.edu>
2019-03-15 11:14:22 -0400

    added media mod

--- jQuery/random-boxes/main.js
+++ jQuery/random-boxes/main.js
@@ -1,3 +1,4 @@
 $(function() {
   // Your interactions go here
+  
 });
--- jQuery/scrolling-events/main.js
+++ jQuery/scrolling-events/main.js
@@ -1,7 +1,3 @@
 $(function() {
   // Your interactions go here
-  $("#picture").click(function(){
-	  $("#bird").animate({left: "40%"});
-  });
- });
-
+});
