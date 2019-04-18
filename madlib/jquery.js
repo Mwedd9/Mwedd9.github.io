4a01cfceacd7d1a18ce8911faca92614cf1e3950
marywedd <weddm037@newschool.edu>
2019-04-17 21:15:48 -0400

    changed data

--- madlib/jquery.js
+++ madlib/jquery.js
@@ -1,16 +1,16 @@
 $(function() {
-	$.get('https://Mwedd9.github.io/api.json', function(data) {
-	console.log(data);
-	var adjective1 = data.adjective1;
+	$.get('https://hoant626.github.io/api.json', function(data) {
+		console.log(data);
+		var adjective1 = data.adjective1;
 
-	$('.word-adjective-1').html(data.adjective1);
-	$('.art-supply').html(data.artSupply);
-	
-
+		$('.word-adjective-1').html(data.adjective1);
+		$('.word-adjective-2').html(data.adjective2);
+		$('.art-supply').html(data.artSupply);
+		$('.computerSoftware').html(data.computerSoftware);
+		$('.word-adjective-2').html(data.adjective2);
+		$('.art-supply').html(data.artSupply);
 	
 	}).fail(function() {
 		console.log ('Error in Get.')
-
-
-		});
 	});
+});
