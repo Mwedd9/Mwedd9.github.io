07e7c35b9ffae7e068adff460ae98f7841530708
marywedd <weddm037@newschool.edu>
2019-04-19 15:54:05 -0400

    add page

--- projects/api/main.js
+++ projects/api/main.js
@@ -1,15 +1,15 @@
 $(function() {
  var endpoints = [
 
-		"https://kquea402.github.io/api.json"
-		"https://glics998.github.io/api.json"
+		*"https://kquea402.github.io/api.json"
+		*"https://glics998.github.io/api.json"
 		"https://colindharrington.github.io/api.json"
 		"https://hoant626.github.io/api.json"
 		"https://karla772.github.io/api.json"
-		"https://leeh779.github.io/api.json"
-		"https://lis874.github.io/api.json"
+		*"https://leeh779.github.io/api.json"
+		*"https://lis874.github.io/api.json"
 		"https://phoebechloee.github.io/api.json"
-		"https://luw779.github.io/api.json"
+		*"https://luw779.github.io/api.json"
 		"https://mullg389.github.io/api.json"
 		"https://pana005.github.io/api.json"
 		"https://raos130.github.io/api.json"
