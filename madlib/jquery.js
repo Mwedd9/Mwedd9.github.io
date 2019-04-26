dfd3ef81ccd5d6b73a51d663c75a9f9e1f2ae241
marywedd <weddm037@newschool.edu>
2019-04-25 21:11:04 -0400

    add page

--- madlib/jquery.js
+++ madlib/jquery.js
@@ -1,22 +1,22 @@
 $(function() {
  var endpoints = [
 
-		"https://kquea402.github.io/api.json"
-		"https://glics998.github.io/api.json"
-		"https://colindharrington.github.io/api.json"
-		"https://hoant626.github.io/api.json"
-		"https://karla772.github.io/api.json"
-		"https://leeh779.github.io/api.json"
-		"https://lis874.github.io/api.json"
-		"https://phoebechloee.github.io/api.json"
-		"https://luw779.github.io/api.json"
-		"https://mullg389.github.io/api.json"
-		"https://pana005.github.io/api.json"
-		"https://raos130.github.io/api.json"
-		"https://samsj948.github.io/api.json"
-		"https://shaha129.github.io/api.json"
-		"https://Mwedd9.github.io/api.json"
-		"https://zhenx804.github.io/api.json"
+		// "https://kquea402.github.io/api.json"
+		// "https://glics998.github.io/api.json"
+		// "https://colindharrington.github.io/api.json"
+		// "https://hoant626.github.io/api.json"
+		// "https://karla772.github.io/api.json"
+		// "https://leeh779.github.io/api.json"
+		// "https://lis874.github.io/api.json"
+		// "https://phoebechloee.github.io/api.json"
+		// "https://luw779.github.io/api.json"
+		// "https://mullg389.github.io/api.json"
+		// "https://pana005.github.io/api.json"
+		// "https://raos130.github.io/api.json"
+		// "https://samsj948.github.io/api.json"
+		// "https://shaha129.github.io/api.json"
+		// "https://Mwedd9.github.io/api.json"
+		// "https://zhenx804.github.io/api.json"
 
  	];
 
@@ -29,9 +29,22 @@ $(function() {
 		$('.word-adjective-1').html(data.adjective1);
 		$('.word-adjective-2').html(data.adjective2);
 		$('.art-supply').html(data.artSupply);
-		$('.computerSoftware').html(data.computerSoftware);
-		$('.word-websiteName').html(data.websiteName);
-		$('.art-supply').html(data.artSupply);
+		$('.computer-software').html(data.computerSoftware);
+		$('.website-name').html(data.websiteName);
+		$('.artist-name').html(data.artistName);
+		$('.celebrity-name').html(data.celebrityName);
+		$('.word-noun-1').html(data.noun1);
+		$('.word-noun-2').html(data.noun2);
+		$('.plural-noun-1').html(data.pluralNoun1);
+		$('.plural-noun-2').html(data.pluralNoun2);
+		$('.plural-noun-3').html(data.pluralNoun3);
+        $('.verb-ing').html(data.verbEndingInIng);
+		$('.place-new-york').html(data.placeInNewYork);
+		$('.body-part').html(data.bodyPart);
+		
+		$('.word-numbers').html(data.numbers);
+
+
 
 
 		$('.foods').html(data.foods.join (', ') );
--- projects/api/main.js
+++ projects/api/main.js
@@ -1,43 +1,36 @@
 $(function() {
- var endpoints = [
 
-		*"https://kquea402.github.io/api.json"
-		*"https://glics998.github.io/api.json"
-		"https://colindharrington.github.io/api.json"
-		"https://hoant626.github.io/api.json"
-		"https://karla772.github.io/api.json"
-		*"https://leeh779.github.io/api.json"
-		*"https://lis874.github.io/api.json"
-		"https://phoebechloee.github.io/api.json"
-		*"https://luw779.github.io/api.json"
-		"https://mullg389.github.io/api.json"
-		"https://pana005.github.io/api.json"
-		"https://raos130.github.io/api.json"
-		"https://samsj948.github.io/api.json"
-		"https://shaha129.github.io/api.json"
-		"https://Mwedd9.github.io/api.json"
-		"https://zhenx804.github.io/api.json"
-
- 	];
 
  	// $('button').click(function())
 
-	$.get('https://Mwedd9.github.io/api.json', function(data) {
+	$.getJSON('https://api.adviceslip.com/advice', function(data) {
 		console.log(data);
-		var adjective1 = data.adjective1;
+		var advice = data.slip.advice;
+		var adviceID = data.slip.slip_id;
 
-		$('.word-adjective-1').html(data.adjective1);
-		$('.word-adjective-2').html(data.adjective2);
-		$('.art-supply').html(data.artSupply);
-		$('.computerSoftware').html(data.computerSoftware);
-		$('.word-websiteName').html(data.websiteName);
-		$('.art-supply').html(data.artSupply);
+		console.log(advice);
 
-
-		$('.foods').html(data.foods.join (', ') );
-
+		$('.advice').html(data.slip.slip_id);
+	
 	
 	}).fail(function() {
 		console.log ('Error in Get.')
+	});
+
+	$('.face').click(function() {
+		$.getJSON('https://api.adviceslip.com/advice', function(data) {
+			console.log(data);
+			var advice = data.slip.advice;
+			var adviceID = data.slip.slip_id;
+
+			console.log(advice);
+
+			$('.advice').html(data.slip.advice);
+		
+
+		
+		}).fail(function() {
+			console.log ('Error in Get.')
+		});
 	});
 });
